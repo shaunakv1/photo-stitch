bee6444a38dc68a51d6d71e8ff1c0153e376f95b
shaunakv1 <shaunakv1@gmail.com>
2015-01-07 23:17:05 -0500

    reduced step size

--- main.js
+++ main.js
@@ -9,15 +9,17 @@ myApp.controller('TilesCtrl', ['$scope', function($scope) {
 	$scope.totalWidth = 625;
 	$scope.totalHeight = 725;
 
+	$scope.step = 47;
 	//$scope.baseUrl = "http://images2.flashphotography.com/Magnifier/MagnifyRender.ashx?O=26529390&R=10001&F=0144&A=71994&rand=0.5268328574020416"
+	//$scope.baseUrl = "http://images2.flashphotography.com/Magnifier/MagnifyRender.ashx?O=26780410&R=10008&F=0046&A=71994&rand=0.5268328574020416"
     $scope.baseUrl = "http://images2.flashphotography.com/Magnifier/MagnifyRender.ashx?O=26529390&R=10008&F=0141&A=71994&rand=0.28094600001350045"
     
     $scope.tiles = [];
 
     stitch();
     function stitch () {
-    	for(var x=0; x <= $scope.totalWidth ; x+=$scope.tileWidth){
-    			for(var y=0; y <= $scope.totalHeight ; y+=$scope.tileHeight){
+    	for(var x=0; x <= $scope.totalWidth ; x+=$scope.step){
+    			for(var y=0; y <= $scope.totalHeight ; y+=$scope.step){
     	    		$scope.tiles.push({
     	    			'x': x,
     	    			'y': y,
